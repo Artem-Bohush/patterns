4b01a2566018d81ec242793ee9bcffae2a67966c
Artem Bohush <art.bohush@gmail.com>
2021-11-12 11:42:40 +0200

    Facade

---
+++ src/structural/facade/classes/subsystem1.class.ts
@@ -0,0 +1,11 @@
+export class Subsystem1 {
+  public initialize(): string {
+    return 'Subsystem1: Ready!\n';
+  }
+
+  // ...
+
+  public operationN(): string {
+    return 'Subsystem1: Go!\n';
+  }
+}
---
+++ src/structural/facade/classes/subsystem2.class.ts
@@ -0,0 +1,11 @@
+export class Subsystem2 {
+  public initialize(): string {
+    return 'Subsystem2: Get ready!\n';
+  }
+
+  // ...
+
+  public operationZ(): string {
+    return 'Subsystem2: Fire!';
+  }
+}
